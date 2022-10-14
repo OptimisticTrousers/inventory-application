9718707dd81d1ec659ffc5c376e72873e31432b1
OptimisticTrousers <tony.isern@yahoo.com>
2022-10-14 19:53:01 -0400

    Making most of the properties in the item schema required

--- models/item.js
+++ models/item.js
@@ -3,11 +3,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema({
-  name: { type: String },
-  description: { type: String },
-  category: { type: Schema.Types.ObjectId, ref: "Category" },
-  price: { type: Number },
-  number_in_stock: { type: Number },
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+  price: { type: Number, required: true },
+  number_in_stock: { type: Number, required: true },
+  size: { type: String },
 });
 
 ItemSchema.virtual("url").get(function () {
