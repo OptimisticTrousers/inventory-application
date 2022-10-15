4ae35fc9f155f94ca8521dc1555fe33606e074b5
OptimisticTrousers <tony.isern@yahoo.com>
2022-10-15 19:50:57 -0400

    Fetching category data in category controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,7 +1,21 @@
+const Category = require("../models/category");
+const Item = require("../models/item")
+
 const index = (req, res, next) => {
-  res.redirect("/")
-}
+  res.redirect("/");
+};
+
+const category_detail = (req, res, next) => {
+  Item.find({}, "name").sort({name: 1}).populate("category").exec(function (err, category_detail) {
+    if(err) {
+      return next(err)
+    }
+
+    res.render("category_detail", {title: `Category List`, category_detail})
+  })
+};
 
 module.exports = {
-  index
-}
+  index,
+  category_detail,
+};
