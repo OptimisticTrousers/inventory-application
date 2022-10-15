a93da87460c13df30b92808b1f46cbc1968d9f72
OptimisticTrousers <tony.isern@yahoo.com>
2022-10-14 21:02:15 -0400

    Writing controller for 'home'

---
+++ controllers/homeController.js
@@ -0,0 +1,28 @@
+const async = require("async");
+const Category = require("../models/category");
+const Item = require("../models/item");
+
+const home_controller = (req, res, next) => {
+  async.parallel(
+    {
+      category_count(callback) {
+        Category.countDocuments({}, callback);
+      },
+      item_count(callback) {
+        Item.countDocuments({}, callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      res.render("index", {
+        title: "NBA Store",
+        error: err,
+        data: results,
+      });
+    }
+  );
+};
+
+module.exports = home_controller;
