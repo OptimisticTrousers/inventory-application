1822200fb9746a371be0006d6022340d53a07830
OptimisticTrousers <tony.isern@yahoo.com>
2022-10-14 21:05:16 -0400

    Using the home controller in routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var home_controller = require("../controllers/homeController")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', home_controller);
 
 module.exports = router;
