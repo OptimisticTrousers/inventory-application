e1ac1a5aefda242390317a6be28c2b7874f1a344
OptimisticTrousers <tony.isern@yahoo.com>
2022-10-14 20:53:13 -0400

    Commenting out catalog and users routes

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/catalog", catalogRouter);
+// app.use("/users", usersRouter);
+// app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
