dd7055f1eb0afb99eef2b95da648f205c340018c
OptimisticTrousers <tony.isern@yahoo.com>
2022-10-15 09:16:43 -0400

    Updating home route module

--- routes/index.js
+++ routes/index.js
@@ -3,62 +3,6 @@ var router = express.Router();
 
 // Require controller modules
 var home_controller = require("../controllers/homeController");
-var category_controller = require("../controllers/categoryController");
-var item_controller = require("../controllers/itemController");
-
-/// CATEGORY ROUTES ///
 
 // GET home page
 router.get("/", home_controller);
-
-// Redirect /category to home page
-router.get("/category", category_controller.index)
-
-// GET request for each Item in a Category
-router.get("/category/:category", category_controller.category_detail)
-
-// GET request for updating a category
-router.get("/category/:category/update", category_controller.category_update_get)
-
-// POST request for updating a category
-router.post("/category/:category/update", category_controller.category_update_post)
-
-// GET request for deleting a category 
-router.get("/category/:category/delete", category_controller.category_delete_get)
-
-// POST request for deleting a category
-router.post("/category/:category/delete", category_controller.category_delete_post)
-
-// GET request for creating a Category
-router.get("/category/create", category_controller.category_create_get);
-
-// POST request for creating a Category
-router.post("/category/create", category_controller.category_create_post);
-
-/// ITEM ROUTES ///
-
-// Redirect the user to the page of Items inside of a Category
-router.get("/category/:category/item", item_controller.index)
-
-// GET request to view Item details
-router.get("/category/:category/item/:id", item_controller.item_detail)
-
-// GET request to create Item
-router.get("/category/:category/item/create", item_controller.item_create_get)
-
-// POST request to create Item
-router.post("/category/:category/item/create", item_controller.item_create_post)
-
-// GET request to update Item
-router.get("/category/:category/item/:id/update", item_controller.item_update_get)
-
-// POST request to update Item
-router.post("/category/:category/item/:id/update", item_controller.item_update_post)
-
-// GET request to delete Item
-router.get("/category/:category/item/:id/delete", item_controller.item_delete_get);
-
-// POST request to delete Item
-router.post("/category/:category/item/:id/delete", item_controller.item_delete_post)
-
-module.exports = router;
