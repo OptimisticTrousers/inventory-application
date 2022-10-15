0da644eac2c37877af588f1652240a556285ddfc
OptimisticTrousers <tony.isern@yahoo.com>
2022-10-15 09:22:21 -0400

    Using only the index router in app.js

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var catalogRouter = require("./routes/catalog");
 
 var app = express();
 
@@ -29,6 +27,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+// app.use("/category", categoryRouter);
+// app.use("/category/:category/item", itemRouter);
 // app.use("/users", usersRouter);
 // app.use("/catalog", catalogRouter);
 
