be316609b925e683a6681a9da13b56da072c9c14
OptimisticTrousers <tony.isern@yahoo.com>
2022-10-15 20:14:52 -0400

    Writing new 'ItemInstance' model

---
+++ models/iteminstance.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ItemInstanceSchema = new Schema({
+  item: { type: Schema.Types.ObjectId, ref: "Item", required: true },
+  size: { type: String },
+  condition: { type: String, required: true },
+  defective: { type: Boolean, required: true },
+  available: { type: Boolean, required: true },
+});
+
+ItemInstanceSchema.virtual("url").get(function () {
+  return `/item/${this._id}`;
+});
+
+module.exports = mongoose.model("ItemInstance", ItemInstanceSchema);
