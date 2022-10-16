e57256fff8ff76c0472d511c2cb2eef6af7b5058
OptimisticTrousers <tony.isern@yahoo.com>
2022-10-16 08:21:34 -0400

    Handling GET request for the 'ItemInstance' detail page

--- controllers/iteminstanceController.js
+++ controllers/iteminstanceController.js
@@ -7,7 +7,25 @@ exports.index = (req, res, next) => {
 }
 
 exports.iteminstance_detail = (req, res, next) => {
+  ItemInstance.findById(req.params.id)
+    .populate("item")
+    .exec((err, iteminstance) => {
+      if(err) {
+        return next(err)
+      }
+      if(iteminstance == null) {
+        // No results.
+        const err = new Error("Item instance not found")
+        err.status = 404
+        return next(err)
+      }
 
+      // Successful, so render
+      res.render("iteminstance_detail", {
+        title: `Instance: ${iteminstance.item.name}`,
+        iteminstance
+      })
+    })
 }
 
 exports.iteminstance_create_get = (req, res, next) => {
