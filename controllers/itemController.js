1fbfc8ee0ead8d24a0bfa42dbce5a4de97f84f86
OptimisticTrousers <tony.isern@yahoo.com>
2022-10-16 08:27:46 -0400

    Adding comments to each of the controllers

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -2,34 +2,42 @@ const Category = require("../models/category")
 const Item = require("../models/item")
 const ItemInstance = require("../models/iteminstance")
 
+// Display list of all Items
 exports.index = (req, res, next) => {
 
 }
 
+// Display detail page for a specific item
 exports.item_detail = (req, res, next) => {
 
 }
 
+// Display item create form on GET
 exports.item_create_get = (req, res, next) => {
 
 }
 
+// Handle book create on POST
 exports.item_create_post = (req, res, next) => {
 
 }
 
+// Display book update form on GET
 exports.item_update_get = (req, res, next) => {
 
 }
 
+// Handle book update on POST
 exports.item_update_post = (req, res, next) => {
 
 }
 
+// Display book delete form on GET
 exports.item_delete_get = (req, res, next) => {
 
 }
 
+// Handle book delete on POST
 exports.item_delete_post = (req, res, next) => {
 
 }
