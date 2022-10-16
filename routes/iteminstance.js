836700c80bd888b0f923ab87b263d9e09f47441d
OptimisticTrousers <tony.isern@yahoo.com>
2022-10-16 14:15:45 -0400

    Creating routing order for 'ItemInstance'

--- routes/iteminstance.js
+++ routes/iteminstance.js
@@ -5,17 +5,17 @@ const item_instance_controller = require("../controllers/iteminstanceController"
 
 /// ITEM INSTANCE ROUTES ///
 
-// GET request for list of all ItemInstance items
-router.get("/", item_instance_controller.index);
-
-// GET request for one ItemInstance
-router.get("/:id", item_instance_controller.iteminstance_detail);
-
 // GET request for creating an ItemInstance
 router.get("/create", item_instance_controller.iteminstance_create_get);
 
 // POST request for creating an ItemInstance
 router.post("/create", item_instance_controller.iteminstance_create_post);
+
+// GET request for list of all ItemInstance items
+router.get("/", item_instance_controller.index);
+
+// GET request for one ItemInstance
+router.get("/:id", item_instance_controller.iteminstance_detail);
 
 // GET request to update an ItemInstance
 router.get("/:id/update", item_instance_controller.iteminstance_update_get);
