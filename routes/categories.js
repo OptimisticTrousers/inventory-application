81703e5eb2b5c88bb274da6f669f6a9ce65bdfd5
OptimisticTrousers <tony.isern@yahoo.com>
2022-10-15 20:46:53 -0400

    Adding an id id in route

--- routes/categories.js
+++ routes/categories.js
@@ -9,19 +9,19 @@ const category_controller = require("../controllers/categoryController")
 router.get("/", category_controller.index)
 
 // GET request for each Item in a Category
-router.get("/:category", category_controller.category_detail)
+router.get("/:id", category_controller.category_detail)
 
 // GET request for updating a category
-router.get("/:category/update", category_controller.category_update_get)
+router.get("/:id/update", category_controller.category_update_get)
 
 // POST request for updating a category
-router.post("/:category/update", category_controller.category_update_post)
+router.post("/:id/update", category_controller.category_update_post)
 
 // GET request for deleting a category 
-router.get("/:category/delete", category_controller.category_delete_get)
+router.get("/:id/delete", category_controller.category_delete_get)
 
 // POST request for deleting a category
-router.post("/:category/delete", category_controller.category_delete_post)
+router.post("/:id/delete", category_controller.category_delete_post)
 
 // GET request for creating a Category
 router.get("/create", category_controller.category_create_get);
