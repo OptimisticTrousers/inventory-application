9d29fa23cb4b08c59922989a5dc95b0a47525ef9
OptimisticTrousers <tony.isern@yahoo.com>
2022-10-15 20:59:01 -0400

    Using exports object

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,11 +1,11 @@
 const Category = require("../models/category");
 const Item = require("../models/item")
 
-const index = (req, res, next) => {
+exports.index = (req, res, next) => {
   res.redirect("/");
 };
 
-const category_detail = (req, res, next) => {
+exports.category_detail = (req, res, next) => {
   Item.find({}, "name").sort({name: 1}).populate("category").exec(function (err, category_detail) {
     if(err) {
       return next(err)
@@ -15,37 +15,27 @@ const category_detail = (req, res, next) => {
   })
 };
 
-const category_update_get = (req, res, next) => {
+exports.category_update_get = (req, res, next) => {
 
 }
 
-const category_update_post = (req, res, next) => {
+exports.category_update_post = (req, res, next) => {
 
 }
 
-const category_delete_get = (req, res, next) => {
+exports.category_delete_get = (req, res, next) => {
 
 }
 
-const category_delete_post = (req, res, next) => {
+exports.category_delete_post = (req, res, next) => {
 
 }
 
-const category_create_get = (req, res, next) => {
+exports.category_create_get = (req, res, next) => {
 
 }
 
-const category_create_post = (req, res, next) => {
+exports.category_create_post = (req, res, next) => {
 
 }
 
-module.exports = {
-  index,
-  category_detail,
-  category_update_get,
-  category_update_post,
-  category_delete_get,
-  category_delete_post,
-  category_create_get,
-  category_create_post
-};
---
+++ controllers/itemController.js
@@ -0,0 +1,35 @@
+const Category = require("../models/category")
+const Item = require("../models/item")
+const ItemInstance = require("../models/iteminstance")
+
+exports.index = (req, res, next) => {
+
+}
+
+exports.item_detail = (req, res, next) => {
+
+}
+
+exports.item_create_get = (req, res, next) => {
+
+}
+
+exports.item_create_post = (req, res, next) => {
+
+}
+
+exports.item_update_get = (req, res, next) => {
+
+}
+
+exports.item_update_post = (req, res, next) => {
+
+}
+
+exports.item_delete_get = (req, res, next) => {
+
+}
+
+exports.item_delete_post = (req, res, next) => {
+
+}
