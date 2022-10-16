a2f7ca299a3c1bb96a1d5b181f6d9138ae740acf
OptimisticTrousers <tony.isern@yahoo.com>
2022-10-16 08:29:55 -0400

    Adding comments to each of the controllers

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,12 +2,14 @@ const Category = require("../models/category");
 const Item = require("../models/item");
 const ItemInstance = require("../models/iteminstance");
 
+// Redirect to the home page
 exports.index = (req, res, next) => {
   res.redirect("/");
 };
 
+// Display detail page for a specific Category
 exports.category_detail = (req, res, next) => {
-  Item.find({}, "name")
+  Item.find({})
     .sort({ name: 1 })
     .populate("category")
     .exec(function (err, category_detail) {
@@ -22,14 +24,23 @@ exports.category_detail = (req, res, next) => {
     });
 };
 
-exports.category_update_get = (req, res, next) => {};
-
-exports.category_update_post = (req, res, next) => {};
-
-exports.category_delete_get = (req, res, next) => {};
-
-exports.category_delete_post = (req, res, next) => {};
-
+// Display Category create form on GET
 exports.category_create_get = (req, res, next) => {};
 
+// Handle Category create on POST
 exports.category_create_post = (req, res, next) => {};
+
+// Display Author update form on GET
+exports.category_update_get = (req, res, next) => {
+  res.render("category_form", {title: "Create Category"})
+};
+
+// Handle Author update on POST
+exports.category_update_post = (req, res, next) => {};
+
+// Display Author delete form on GET
+exports.category_delete_get = (req, res, next) => {};
+
+// Handle Author delete on POST
+exports.category_delete_post = (req, res, next) => {};
+
