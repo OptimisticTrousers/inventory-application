21c6e219055c053d079910f656394a2b9e0e7b39
OptimisticTrousers <tony.isern@yahoo.com>
2022-10-16 08:59:11 -0400

    Handling GET request to return all Item items

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,43 +1,38 @@
-const Category = require("../models/category")
-const Item = require("../models/item")
-const ItemInstance = require("../models/iteminstance")
+const Category = require("../models/category");
+const Item = require("../models/item");
+const ItemInstance = require("../models/iteminstance");
 
 // Display list of all Items
 exports.index = (req, res, next) => {
+  Item.find({}, "name category")
+    .sort({ name: 1 })
+    .populate("category")
+    .exec((err, list_items) => {
+      if (err) {
+        return next(err);
+      }
 
-}
+      res.render("item_list", { title: "Item List", item_list: list_items });
+    });
+};
 
 // Display detail page for a specific item
-exports.item_detail = (req, res, next) => {
-
-}
+exports.item_detail = (req, res, next) => {};
 
 // Display item create form on GET
-exports.item_create_get = (req, res, next) => {
-
-}
+exports.item_create_get = (req, res, next) => {};
 
 // Handle book create on POST
-exports.item_create_post = (req, res, next) => {
-
-}
+exports.item_create_post = (req, res, next) => {};
 
 // Display book update form on GET
-exports.item_update_get = (req, res, next) => {
-
-}
+exports.item_update_get = (req, res, next) => {};
 
 // Handle book update on POST
-exports.item_update_post = (req, res, next) => {
-
-}
+exports.item_update_post = (req, res, next) => {};
 
 // Display book delete form on GET
-exports.item_delete_get = (req, res, next) => {
-
-}
+exports.item_delete_get = (req, res, next) => {};
 
 // Handle book delete on POST
-exports.item_delete_post = (req, res, next) => {
-
-}
+exports.item_delete_post = (req, res, next) => {};
