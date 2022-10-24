b387b3606a2d9f113a0ebcd15b510d8e7a425a9e
OptimisticTrousers <tony.isern@yahoo.com>
2022-10-24 18:06:31 -0400

    Removing 'defective' attribute on ItemInstance schema

--- models/iteminstance.js
+++ models/iteminstance.js
@@ -5,7 +5,6 @@ const ItemInstanceSchema = new Schema({
   item: { type: Schema.Types.ObjectId, ref: "Item", required: true },
   size: { type: String },
   condition: { type: String, required: true },
-  defective: { type: Boolean, required: true },
   available: { type: Boolean, required: true },
 });
 
