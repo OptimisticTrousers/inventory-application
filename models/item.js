fef5e45b0ebdd9c9d0a01de968bd33acfd30f24f
OptimisticTrousers <tony.isern@yahoo.com>
2022-10-25 11:20:57 -0400

    Allowing users to add multiple categories to an Item

--- models/item.js
+++ models/item.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const ItemSchema = new Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
-  category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+  category: [{ type: Schema.Types.ObjectId, ref: "Category", required: true }],
   price: { type: Number, required: true },
   sizes: { type: Array },
 });
