472a401c77e4b870142f32e1d378ecdb412596dd
OptimisticTrousers <tony.isern@yahoo.com>
2022-11-08 06:00:17 -0500

    Multer setup

--- routes/category.js
+++ routes/category.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const upload = multer({ dest: "public/uploads/" });
+const upload = multer({ dest: "public/images/" });
 
 const category_controller = require("../controllers/categoryController");
 
--- routes/item.js
+++ routes/item.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const upload = multer({
-  dest: "public/uploads/",
+  dest: "public/images/",
 });
 
 const item_controller = require("../controllers/itemController");
