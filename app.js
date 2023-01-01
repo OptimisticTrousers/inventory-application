1b9396ce6ad7bb602e53acd561af4ead5fbfe4fa
OptimisticTrousers <tony.isern1@gmail.com>
2023-01-01 18:44:57 -0500

    Small changes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+
 require("dotenv").config();
 
 const indexRouter = require("./routes/index");
